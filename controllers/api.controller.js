94a01fc0cc176a608f7eb47e14f4adb9b45d1b22
MKD <kabir.dastgir@hotmail.com>
2022-01-20 17:37:20 +0000

    refactor api endpoint

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 exports.getApi = (req, res) => {
   return fs.readFile('endpoints.json', 'utf-8', (err, data) => {
-    return res.status(200).send(JSON.parse({ data }));
+    return res.status(200).send(JSON.parse(data));
   });
 };
