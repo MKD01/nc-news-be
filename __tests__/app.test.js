085df6f91a13c1faf08891e4b569eb35fedfbee7
MKD <kabir.dastgir@hotmail.com>
2022-01-21 13:25:52 +0000

    format testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,19 @@ const seed = require('../db/seeds/seed.js');
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
+describe('/api', () => {
+  describe('GET', () => {
+    test('Return status code 200 and read conetents of endpoints.json', () => {
+      return request(app)
+        .get('/api')
+        .expect(200)
+        .then(({ body }) => {
+          expect(typeof body).toBe('object');
+        });
+    });
+  });
+});
+
 describe('/api/topics', () => {
   describe('GET', () => {
     test('Return status code 200 and an object with key topics and value of all the topics', () => {
@@ -26,112 +39,6 @@ describe('/api/topics', () => {
           } else {
             fail('Error, Body Is Empty');
           }
-        });
-    });
-  });
-});
-
-describe('/api/articles/:article_id', () => {
-  describe('GET', () => {
-    test('Return status code 200 and the correct comment count', () => {
-      return request(app)
-        .get('/api/articles/1')
-        .expect(200)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-          expect(body.article).toEqual({
-            article_id: 1,
-            title: 'Living in the shadow of a great man',
-            body: 'I find this existence challenging',
-            votes: 100,
-            author: 'butter_bridge',
-            topic: 'mitch',
-            created_at: '2020-07-09T20:11:00.000Z',
-            comment_count: '11',
-          });
-        });
-    });
-
-    test('Return status code 404 with an error message if article_id is valid but not found', () => {
-      return request(app)
-        .get('/api/articles/300000')
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Not found');
-        });
-    });
-
-    test('Return status code 400 with error message if article_id is invalid', () => {
-      return request(app)
-        .get('/api/articles/a')
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Bad request');
-        });
-    });
-  });
-
-  describe('PATCH', () => {
-    test('Return status code 201, increment votes by 10 and return object', () => {
-      const increaseVotes = { inc_votes: 10 };
-      return request(app)
-        .patch('/api/articles/1')
-        .send(increaseVotes)
-        .expect(201)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-          expect(body.article).toEqual({
-            article_id: 1,
-            title: 'Living in the shadow of a great man',
-            body: 'I find this existence challenging',
-            votes: 110,
-            author: 'butter_bridge',
-            topic: 'mitch',
-            created_at: '2020-07-09T20:11:00.000Z',
-          });
-        });
-    });
-
-    test('Return status code 201, decrement votes by 10 and return object', () => {
-      const decreaseVotes = { inc_votes: -10 };
-      return request(app)
-        .patch('/api/articles/1')
-        .send(decreaseVotes)
-        .expect(201)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-          expect(body.article).toEqual({
-            article_id: 1,
-            title: 'Living in the shadow of a great man',
-            body: 'I find this existence challenging',
-            votes: 90,
-            author: 'butter_bridge',
-            topic: 'mitch',
-            created_at: '2020-07-09T20:11:00.000Z',
-          });
-        });
-    });
-
-    test('Return status code 404 with an error message if article_id is valid but not found', () => {
-      const increaseVotes = { inc_votes: 10 };
-
-      return request(app)
-        .patch('/api/articles/300000')
-        .send(increaseVotes)
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Not found');
-        });
-    });
-
-    test('Return status code 400 with error message if article_id is invalid', () => {
-      const increaseVotes = { inc_votes: 10 };
-      return request(app)
-        .patch('/api/articles/a')
-        .send(increaseVotes)
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Bad request');
         });
     });
   });
@@ -238,94 +145,200 @@ describe('/api/articles', () => {
         });
     });
   });
-});
 
-describe('/api/articles/:article_id/comments', () => {
-  describe('GET', () => {
-    test('Return status code 200 with an array of comments for the given article id', () => {
-      return request(app)
-        .get('/api/articles/1/comments')
-        .expect(200)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-          body.comments.forEach((comment) => {
-            expect(comment).toMatchObject({
-              comment_id: expect.any(Number),
-              votes: expect.any(Number),
-              created_at: expect.any(String),
-              body: expect.any(String),
-              author: expect.any(String),
+  describe('/api/articles/:article_id', () => {
+    describe('GET', () => {
+      test('Return status code 200 and the correct comment count', () => {
+        return request(app)
+          .get('/api/articles/1')
+          .expect(200)
+          .then(({ body }) => {
+            expect(typeof body).toBe('object');
+            expect(body.article).toEqual({
+              article_id: 1,
+              title: 'Living in the shadow of a great man',
+              body: 'I find this existence challenging',
+              votes: 100,
+              author: 'butter_bridge',
+              topic: 'mitch',
+              created_at: '2020-07-09T20:11:00.000Z',
+              comment_count: '11',
             });
           });
-        });
-    });
+      });
 
-    test('Return status code 400 if article_id is not valid', () => {
-      return request(app)
-        .get('/api/articles/apple/comments')
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Bad request');
-        });
-    });
-
-    test('Return status code 404 if article_id is valid but no comments have that given article_id', () => {
-      return request(app)
-        .get('/api/articles/8/comments')
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Not found');
-        });
-    });
-  });
-
-  describe('POST', () => {
-    test('Return status code 201 with the new comment', () => {
-      const newComment = {
-        username: 'butter_bridge',
-        body: 'an interesting story',
-      };
-      return request(app)
-        .post('/api/articles/1/comments')
-        .send(newComment)
-        .expect(201)
-        .then(({ body }) => {
-          expect(body.comment).toEqual({
-            comment_id: expect.any(Number),
-            votes: expect.any(Number),
-            created_at: expect.any(String),
-            body: 'an interesting story',
-            author: 'butter_bridge',
+      test('Return status code 404 with an error message if article_id is valid but not found', () => {
+        return request(app)
+          .get('/api/articles/300000')
+          .expect(404)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Not found');
           });
-        });
+      });
+
+      test('Return status code 400 with error message if article_id is invalid', () => {
+        return request(app)
+          .get('/api/articles/a')
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Bad request');
+          });
+      });
     });
 
-    test('Return status code 400 if article_id is not valid', () => {
-      const newComment = {
-        username: 'butter_bridge',
-        body: 'an interesting story',
-      };
-      return request(app)
-        .post('/api/articles/apple/comments')
-        .send(newComment)
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Bad request');
-        });
+    describe('PATCH', () => {
+      test('Return status code 201, increment votes by 10 and return object', () => {
+        const increaseVotes = { inc_votes: 10 };
+        return request(app)
+          .patch('/api/articles/1')
+          .send(increaseVotes)
+          .expect(201)
+          .then(({ body }) => {
+            expect(typeof body).toBe('object');
+            expect(body.article).toEqual({
+              article_id: 1,
+              title: 'Living in the shadow of a great man',
+              body: 'I find this existence challenging',
+              votes: 110,
+              author: 'butter_bridge',
+              topic: 'mitch',
+              created_at: '2020-07-09T20:11:00.000Z',
+            });
+          });
+      });
+
+      test('Return status code 201, decrement votes by 10 and return object', () => {
+        const decreaseVotes = { inc_votes: -10 };
+        return request(app)
+          .patch('/api/articles/1')
+          .send(decreaseVotes)
+          .expect(201)
+          .then(({ body }) => {
+            expect(typeof body).toBe('object');
+            expect(body.article).toEqual({
+              article_id: 1,
+              title: 'Living in the shadow of a great man',
+              body: 'I find this existence challenging',
+              votes: 90,
+              author: 'butter_bridge',
+              topic: 'mitch',
+              created_at: '2020-07-09T20:11:00.000Z',
+            });
+          });
+      });
+
+      test('Return status code 404 with an error message if article_id is valid but not found', () => {
+        const increaseVotes = { inc_votes: 10 };
+
+        return request(app)
+          .patch('/api/articles/300000')
+          .send(increaseVotes)
+          .expect(404)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Not found');
+          });
+      });
+
+      test('Return status code 400 with error message if article_id is invalid', () => {
+        const increaseVotes = { inc_votes: 10 };
+        return request(app)
+          .patch('/api/articles/a')
+          .send(increaseVotes)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Bad request');
+          });
+      });
     });
 
-    test('Return status code 404 if username is not valid', () => {
-      const newComment = {
-        username: 'notAUser',
-        body: 'an interesting story',
-      };
-      return request(app)
-        .post('/api/articles/8/comments')
-        .send(newComment)
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Bad request');
+    describe('/api/articles/:article_id/comments', () => {
+      describe('GET', () => {
+        test('Return status code 200 with an array of comments for the given article id', () => {
+          return request(app)
+            .get('/api/articles/1/comments')
+            .expect(200)
+            .then(({ body }) => {
+              expect(typeof body).toBe('object');
+              body.comments.forEach((comment) => {
+                expect(comment).toMatchObject({
+                  comment_id: expect.any(Number),
+                  votes: expect.any(Number),
+                  created_at: expect.any(String),
+                  body: expect.any(String),
+                  author: expect.any(String),
+                });
+              });
+            });
         });
+
+        test('Return status code 400 if article_id is not valid', () => {
+          return request(app)
+            .get('/api/articles/apple/comments')
+            .expect(400)
+            .then(({ body }) => {
+              expect(body.msg).toBe('Bad request');
+            });
+        });
+
+        test('Return status code 404 if article_id is valid but no comments have that given article_id', () => {
+          return request(app)
+            .get('/api/articles/8/comments')
+            .expect(404)
+            .then(({ body }) => {
+              expect(body.msg).toBe('Not found');
+            });
+        });
+      });
+
+      describe('POST', () => {
+        test('Return status code 201 with the new comment', () => {
+          const newComment = {
+            username: 'butter_bridge',
+            body: 'an interesting story',
+          };
+          return request(app)
+            .post('/api/articles/1/comments')
+            .send(newComment)
+            .expect(201)
+            .then(({ body }) => {
+              expect(body.comment).toEqual({
+                comment_id: expect.any(Number),
+                votes: expect.any(Number),
+                created_at: expect.any(String),
+                body: 'an interesting story',
+                author: 'butter_bridge',
+              });
+            });
+        });
+
+        test('Return status code 400 if article_id is not valid', () => {
+          const newComment = {
+            username: 'butter_bridge',
+            body: 'an interesting story',
+          };
+          return request(app)
+            .post('/api/articles/apple/comments')
+            .send(newComment)
+            .expect(400)
+            .then(({ body }) => {
+              expect(body.msg).toBe('Bad request');
+            });
+        });
+
+        test('Return status code 404 if username is not valid', () => {
+          const newComment = {
+            username: 'notAUser',
+            body: 'an interesting story',
+          };
+          return request(app)
+            .post('/api/articles/8/comments')
+            .send(newComment)
+            .expect(400)
+            .then(({ body }) => {
+              expect(body.msg).toBe('Bad request');
+            });
+        });
+      });
     });
   });
 });
@@ -350,68 +363,7 @@ describe('/api/comments/:comment_id', () => {
         });
     });
   });
-});
 
-describe('/api', () => {
-  describe('GET', () => {
-    test('Return status code 200 and read conetents of endpoints.json', () => {
-      return request(app)
-        .get('/api')
-        .expect(200)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-        });
-    });
-  });
-});
-
-describe('/api/users', () => {
-  describe('GET', () => {
-    test('Return status code 200 with an array of objects containing the username of each user', () => {
-      return request(app)
-        .get('/api/users')
-        .expect(200)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-          body.users.forEach((user) => {
-            expect(user).toMatchObject({
-              username: expect.any(String),
-            });
-          });
-        });
-    });
-  });
-});
-
-describe('/api/users/:username', () => {
-  describe('GET', () => {
-    test('Return status code 200 with an array of objects containing the username of each user', () => {
-      return request(app)
-        .get('/api/users/butter_bridge')
-        .expect(200)
-        .then(({ body }) => {
-          expect(typeof body).toBe('object');
-          expect(body.user).toMatchObject({
-            username: 'butter_bridge',
-            name: 'jonny',
-            avatar_url:
-              'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
-          });
-        });
-    });
-
-    test('Return status code 404 if username is not valid', () => {
-      return request(app)
-        .get('/api/users/notAValidUserName')
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toBe('Not found');
-        });
-    });
-  });
-});
-
-describe('/api/comments/:comment_id', () => {
   describe('PATCH', () => {
     test('Return status code 201, increment votes by 1 and return object', () => {
       const increaseVotes = { inc_votes: 1 };
@@ -460,6 +412,52 @@ describe('/api/comments/:comment_id', () => {
         .then(({ body }) => {
           expect(body.msg).toBe('Not found');
         });
+    });
+  });
+});
+
+describe('/api/users', () => {
+  describe('GET', () => {
+    test('Return status code 200 with an array of objects containing the username of each user', () => {
+      return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({ body }) => {
+          expect(typeof body).toBe('object');
+          body.users.forEach((user) => {
+            expect(user).toMatchObject({
+              username: expect.any(String),
+            });
+          });
+        });
+    });
+  });
+
+  describe('/api/users/:username', () => {
+    describe('GET', () => {
+      test('Return status code 200 with an array of objects containing the username of each user', () => {
+        return request(app)
+          .get('/api/users/butter_bridge')
+          .expect(200)
+          .then(({ body }) => {
+            expect(typeof body).toBe('object');
+            expect(body.user).toMatchObject({
+              username: 'butter_bridge',
+              name: 'jonny',
+              avatar_url:
+                'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
+            });
+          });
+      });
+
+      test('Return status code 404 if username is not valid', () => {
+        return request(app)
+          .get('/api/users/notAValidUserName')
+          .expect(404)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Not found');
+          });
+      });
     });
   });
 });
